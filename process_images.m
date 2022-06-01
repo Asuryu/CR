cd1b99775171c2557a151acc9e1e041130245977
Tomás Silva <tomas-250@outlook.com>
2022-06-02 00:36:29 +0100

    Função para processar as imagens
    Dá resize, transforma em matriz binária e adiciona a imagem à matriz de imagens.

---
+++ process_images.m
@@ -0,0 +1,24 @@
+function [input, size] = process_images(folder)
+
+directory = dir(folder);
+size = length(directory);
+
+for i = 1 : length(directory)
+    % Read image
+    img = imread(append(directory(i).folder, "\", directory(i).name));
+    img = img(:,:,1);
+    
+    % Rezise image from 224px to 32px
+    img = imresize(img, [56, 56]);
+    
+    % Convert image in binary matrix
+    img = imbinarize(img);
+    
+    % Transforma a matriz numa coluna
+    img = img(:);
+    
+    % Adiciona a coluna aos dados
+    input(:, i) = img; 
+end
+
+end
