86f1c0b00539b0d5d2d05fc684799c11ed684f90
Asuryu <tomas-250@outlook.com>
2022-06-02 00:55:46 +0100

    Update gen_target.m

--- gen_target.m
+++ gen_target.m
@@ -1,15 +1,6 @@
 function [target] = gen_target(tamanho)
 
-target = zeros(10, tamanho);
-n = tamanho / 10;
-
-linha = 1;
-
-for i = 1 : tamanho
-    target(linha, i) = 1;
-    if rem(i, n) == 0
-        linha = linha + 1;
-    end
-end
+target = zeros(5, tamanho);
+% Não sabemos o que meter aqui
 
 end
