7aedadddcbfad990ef6081346529e347642d9368
Asuryu <tomas-250@outlook.com>
2022-06-04 03:09:00 +0100

    Gerar a matriz alvo (target)

--- gen_target.m
+++ gen_target.m
@@ -1,6 +1,18 @@
 function [target] = gen_target(tamanho)
 
-target = zeros(5, tamanho);
+%target = zeros(5, tamanho);
 % Não sabemos o que meter aqui
+
+target = zeros(6, tamanho);
+n = tamanho / 6;
+
+linha = 1;
+
+for i = 1 : tamanho
+    target(linha, i) = 1;
+    if rem(i, n) == 0
+        linha = linha + 1;
+    end
+end
 
 end
