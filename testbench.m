9e498f0cf17b3ce2dce60d52a28e38749f5e634b
Asuryu <tomas-250@outlook.com>
2022-06-05 04:21:38 +0100

    Update testbench.m

--- testbench.m
+++ testbench.m
@@ -1,20 +1,2 @@
-clear all
-close all
-
-% Configuração da rede
-net = feedforwardnet(10);
-net.layers{1}.transferFcn = 'logsig';
-net.trainFcn = "trainlm";
-net.divideFcn = "";
-net.trainParam.epochs = 1000;
-
-pasta = "imagens\start\*\*.png";
-
-[input, tamanho] = process_images(pasta);
-target = gen_target(tamanho);
-
-%[net, tr] = train(net, input, target);
-%view(net);
-
-% out = sim(net, input);
-% plotconfusion(target, out);
+loadedNet = load("best_nn.mat");
+var = loadedNet.net.dimensions;
