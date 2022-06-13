9aed6437d5995027b1f8007a3214f70040d020d9
Asuryu <tomas-250@outlook.com>
2022-06-13 05:31:11 +0100

    Conclusão do trabalho de CR!

--- process_images.m
+++ process_images.m
@@ -4,14 +4,14 @@ directory = dir(folder);
 size = length(directory);
 
 for i = 1 : length(directory)
-    % Read image
+    % Lê a imagem
     img = imread(append(directory(i).folder, "/", directory(i).name));
     img = img(:,:,1);
     
-    % Rezise image from 224px to 32px
+    % Redimensiona a imagem para 32px por 32px
     img = imresize(img, [32, 32]);
     
-    % Convert image in binary matrix
+    % Converte a imagem para uma matriz binária
     img = imbinarize(img);
     
     % Transforma a matriz numa coluna
